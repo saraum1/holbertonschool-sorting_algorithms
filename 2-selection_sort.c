d1d38040ff00f562fc12b181c89a2827064090ad
root <root@ip-10-42-152-116.ec2.internal>
2025-10-28 11:16:11 +0000

    Fix: selection sort function and add big O comments

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,23 @@
 #include "sort.h"
-/**
- * selection_sort - sorts an array of ints in ascending order using Selection sort
- * @array: pointer to array
- * @size: number of elements in array
- */
+
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_i;
-int tmp;
-if (array == NULL || size < 2)
-return;
+size_t i, j, min;
+int temp;
+
 for (i = 0; i < size - 1; i++)
 {
-min_i = i;
+min = i;
 for (j = i + 1; j < size; j++)
 {
-if (array[j] < array[min_i])
-min_i = j;
+if (array[j] < array[min])
+min = j;
 }
-if (min_i != i)
+if (min != i)
 {
-tmp = array[i];
-array[i] = array[min_i];
-array[min_i] = tmp;
+temp = array[i];
+array[i] = array[min];
+array[min] = temp;
 print_array(array, size);
 }
 }
