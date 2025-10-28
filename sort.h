b685c7761048ac7049faf435b3851a9ff868f971
root <root@ip-10-42-152-116.ec2.internal>
2025-10-28 11:30:04 +0000

    Quick sort using Lomuto partition + Big O

--- sort.h
+++ sort.h
@@ -25,6 +25,6 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-
+static void swap_ints(int *a, int *b, int *array, size_t size);
 
 #endif /* SORT_H */
