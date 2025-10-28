b62e5570600226239d33d0f7cbd041dbb59c46c1
root <root@ip-10-42-152-116.ec2.internal>
2025-10-28 11:52:10 +0000

    Fixed quick sort starting from column 1 for Betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,52 +1,83 @@
 #include "sort.h"
 
-void swap_ints(int *a, int *b, int *array, size_t size)
+/**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+static void swap_ints(int *a, int *b)
 {
-void quick_sort(int *array, size_t size)
-{
-if (array == NULL || size < 2)
-return;
 int tmp;
-if (a == b)
-return;
+
 tmp = *a;
 *a = *b;
 *b = tmp;
-print_array(array, size);
 }
 
+/**
+ * lomuto_partition - Order a subset of an array using Lomuto partition.
+ * @array: The array of integers.
+ * @low: The starting index of the subset.
+ * @high: The ending index of the subset.
+ * @size: The size of the array.
+ *
+ * Return: The final partition index.
+ */
 static int lomuto_partition(int *array, int low, int high, size_t size)
 {
 int pivot = array[high];
-int i = low - 1;
-int j;
+int i = low, j;
+
 for (j = low; j < high; j++)
 {
 if (array[j] < pivot)
 {
+if (i != j)
+{
+swap_ints(&array[i], &array[j]);
+print_array(array, size);
+}
 i++;
-swap_ints(&array[i], &array[j], array, size);
 }
 }
-i++;
-swap_ints(&array[i], &array[high], array, size);
+
+if (array[i] > pivot)
+{
+swap_ints(&array[i], &array[high]);
+print_array(array, size);
+}
+
 return (i);
 }
 
-static void quicksort_rec(int *array, int low, int high, size_t size)
+/**
+ * quick_sort_rec - Recursive Quick sort using Lomuto partition scheme.
+ * @array: Array of integers.
+ * @low: Starting index.
+ * @high: Ending index.
+ * @size: Size of the array.
+ */
+static void quick_sort_rec(int *array, int low, int high, size_t size)
 {
-int p;
+int pivot;
+
 if (low < high)
 {
-p = lomuto_partition(array, low, high, size);
-quicksort_rec(array, low, p - 1, size);
-quicksort_rec(array, p + 1, high, size);
+pivot = lomuto_partition(array, low, high, size);
+quick_sort_rec(array, low, pivot - 1, size);
+quick_sort_rec(array, pivot + 1, high, size);
 }
 }
 
+/**
+ * quick_sort - Sort an array of integers in ascending order.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ */
 void quick_sort(int *array, size_t size)
 {
-if (!array || size < 2)
+if (array == NULL || size < 2)
 return;
-quicksort_rec(array, 0, (int)size - 1, size);
+
+quick_sort_rec(array, 0, size - 1, size);
 }
